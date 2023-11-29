1debfb8029b8672e3d60275e23066f2485798185
prasanna <techvlogshyd@gmail.com>
2023-11-29 23:31:19 +0530

    add interview questions

---
+++ src/interview/arcesium/Triplets.java
@@ -0,0 +1,98 @@
+package interview.arcesium;
+
+/*
+1. JSON object Jason Path gson
+2. Cypress Vs Selenium
+3. E2E CI CD flow
+4. Linus commands
+5. API methods
+6. Triplets e.g. sum 8 input [1, 2 ,3, 8, 5]
+############################
+
+java package
+
+com.arcesium
+
+
+com.arcesium.App1   --> add(1,2)
+com.arcesium.App2
+....
+com.arcesium.App100
+
+
+
+@Test
+void addTest{
+	assert add(1,2).equals(3)
+}
+
+
+getPackages = ArrayList<>();
+for(String package : getPackages){
+	@Test()
+  validateMethod1(){
+  		Assert.assertEquals(subtraction(5-2),3);
+  }
+
+}
+#####################################
+
+
+[1,4,8,9,15,3]
+
+sum = 8
+find all triplets whose sum == 8
+
+(1,4,3)
+
+findTriplet(int[]  input, int sum){
+	Map<Integer, integer> indexes = new HashMap<>();
+  for(int i =0; i<length; i++){
+
+    indexes.put(input[i], i);
+    int subSum = sum - no;
+  }
+}
+
+######################################
+
+jsonpath
+
+/getUser>id=12345
+
+
+{
+nested. []
+}
+
+#######################################
+
+Devops
+
+
+######################################
+Linux
+
+######################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
+public class Triplets {
+
+}
