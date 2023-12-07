53136e1f5af2449fb8650dbe4cbcb0b5bb6a86ca
prasanna <techvlogshyd@gmail.com>
2023-12-07 08:32:59 +0530

    add interview questions

--- src/interview/servicenow/SubsetsOfString.java
+++ src/interview/servicenow/SubsetsOfString.java
@@ -8,17 +8,33 @@ public class SubsetsOfString {
     public static void main(String[] args) {
         List<String> result = new ArrayList<>();
         String input = "ABCDEF";
-        generateSubset(input, "", 1, result);
+        generateSubset(input, 0, 0, result);
         System.out.println(result);
+        List<String> result2 = new ArrayList<>();
+        String input2 = "ABCDEF";
+        generateSubset(input2, result2);
+        System.out.println(result2);
     }
 
-    static void generateSubset(String input, String subSet, int index, List<String> result) {
-        result.add(subSet);
-
-        for (int i = index; i < input.length(); i++) {
-            generateSubset(input, subSet + input.charAt(i), i + 1, result);
+    static void generateSubset(String input, int start, int end, List<String> result) {
+        if (start == input.length()) {
+            return;
         }
 
+        if (end == input.length()) {
+            generateSubset(input, start + 1, start + 1, result);
+        } else {
+            result.add(input.substring(start, end + 1));
+            generateSubset(input, start, end + 1, result);
+        }
+
+    }
+    static void generateSubset(String input, List<String> result) {
+        for (int i = 0; i < input.length(); i++) {
+            for (int j = i; j < input.length(); j++) {
+                result.add(input.substring(i,j+1));
+            }
+        }
     }
 
 }
