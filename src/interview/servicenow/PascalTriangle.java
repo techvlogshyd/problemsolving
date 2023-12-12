76a9328031a77c9cc195a01e9da8181c36cf5659
prasanna <techvlogshyd@gmail.com>
2023-12-12 23:09:02 +0530

    add interview questions

--- src/interview/servicenow/PascalTriangle.java
+++ src/interview/servicenow/PascalTriangle.java
@@ -6,21 +6,24 @@ import java.util.List;
 public class PascalTriangle {
     public static void main(String[] args) {
         System.out.println(printPascalTriangle(4));
+        System.out.println(printPascalTriangle(5));
+        System.out.println(printPascalTriangle(10));
     }
-    static List<List<Integer>> printPascalTriangle(int numRows){
+
+    static List<List<Integer>> printPascalTriangle(int numRows) {
         List<List<Integer>> res = new ArrayList<>();
 
-        for(int i=0; i<numRows; i++){
-            List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
 
-            for(int j=0; j<=i; j++){
-                if(j>0 && i>0 && res.get(i-1).size()-1>=j){
-                    list.add(res.get(i-1).get(j-1)+res.get(i-1).get(j));
-                } else{
-                    list.add(1);
+            for (int j = 0; j <= i; j++) {
+                if (j > 0 && i > 0 && j < res.get(i - 1).size()) {
+                    row.add(res.get(i - 1).get(j - 1) + res.get(i - 1).get(j));
+                } else {
+                    row.add(1);
                 }
             }
-            res.add(list);
+            res.add(row);
         }
 
         return res;
